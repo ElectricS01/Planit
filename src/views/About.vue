ebc2bff76a2664ba61845d1c8ab859a2ee330deb
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2024-06-03 11:19:59 +1000

    Update logo and favicon

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="container-flex">
     <div class="menu">
       <p class="title-menu">About Planit</p>
+      <img src="../assets/logo.png" height="144" width="144" alt="Logo" />
       <p class="small">
         Planit is a modern and advanced project management software solution
       </p>
